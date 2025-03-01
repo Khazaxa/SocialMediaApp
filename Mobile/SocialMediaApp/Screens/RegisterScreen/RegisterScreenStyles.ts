27a9bab0a60e5463e35440c654ba377126c0c925
BacaKamil <kamil.baca@outlook.com>
2025-03-01 19:05:43 +0100

    register view

--- Mobile/SocialMediaApp/Screens/RegisterScreen/RegisterScreenStyles.ts
+++ Mobile/SocialMediaApp/Screens/RegisterScreen/RegisterScreenStyles.ts
@@ -5,6 +5,30 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-start",
+    marginTop: 50,
+  },
+  input: {
+    height: 50,
+    width: 300,
+    padding: 10,
+    marginVertical: 5,
+    borderWidth: 2,
+    borderColor: "#ccc",
+    borderRadius: 100,
+    textAlign: "center",
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 18,
+  },
+  registerBtn: {
+    height: 50,
+    width: 300,
+    backgroundColor: "#28A745",
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
   },
 });
 
