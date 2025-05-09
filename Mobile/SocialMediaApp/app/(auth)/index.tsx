6783bd6b5992a5d04c9091f6a1c87c2507c09b52
Kobbe <korbanj1108@gmail.com>
2025-05-09 14:12:15 +0200

    Revert "Merge pull request #47 from Khazaxa/baca"
    This reverts commit 3dbd0244be1f13529874dca9eb8ba537dbde1adf, reversing
    changes made to 9890db77fb2f79674cda5df94f03921e65c40d83.

--- Mobile/SocialMediaApp/app/(auth)/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from 'expo-router';
-import { useAuthStore } from '@/store/authStore';
-
-export default function AuthIndex() {
-  const { isAuthenticated } = useAuthStore();
-  
-  return isAuthenticated ? <Redirect href="/(tabs)" /> : <Redirect href="/login" />;
-}
